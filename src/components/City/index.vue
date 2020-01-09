6e209bcb01eff6b7637b21002072670b9dcb0b77
houqiyuan <1115080155@qq.com>
2020-01-09 21:48:53 +0800

    add setData

--- src/components/City/index.vue
+++ src/components/City/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="city_body">
-        <div class="city_list">
+        <!-- <div class="city_list">
             <div class="city_hot">
                 <h2>热门城市</h2>
                 <ul class="clearfix">
@@ -80,13 +80,104 @@
                 <li>D</li>
                 <li>E</li>
             </ul>
+        </div> -->
+        <div class="city_list">
+            <div class="city_hot">
+                <h2>热门城市</h2>
+                <ul class="clearfix">
+                    <li v-for="item in hotList" :key="item.id">{{item.nm}}</li>
+                </ul>
+            </div>
+            <div class="city_sort" ref="city_sort">
+                <div v-for="item in cityList" :key="item.index">
+                    <h2>{{item.index}}</h2>
+                    <ul>
+                        <li v-for="itemList in item.list" :key="itemList.id">{{itemList.nm}}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="city_index">
+            <ul>
+                <li v-for="(item,index) in cityList" :key="item.index" @touchstart="handleToIndex(index)">{{item.index}}</li>
+            </ul>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    name:'City'
+    
+    name:'City',
+    data(){
+        return{
+            cityList:[],
+            hotList:[]
+        }
+    },
+    mounted(){
+        this.axios.get('/api/cityList').then((res)=>{
+            var msg=res.data.msg;
+            if(msg==='ok'){
+                var cities=res.data.data.cities;
+                var {cityList,hotList}=  this.formatCityList(cities)
+                this.cityList=cityList;
+                this.hotList=hotList;
+            }
+        });
+    },
+    methods:{
+        formatCityList(cities){
+            var cityList=[];
+            var hotList=[];
+            for(var a=0;a<cities.length;a++){
+                if(cities[a].isHot===1){
+                    hotList.push(cities[a]);
+                }
+            }
+
+            for(var i=0;i<cities.length;i++){
+                var fristLetter=cities[i].py.substring(0,1).toUpperCase();
+                if(toCom(fristLetter)){  //新添加index
+                    cityList.push({index:fristLetter,list:[{nm:cities[i].nm,id:cities[i].id}]})
+                }else{   //累加到已有index中
+                    for(var j=0;j<cityList.length;j++){
+                        if(cityList[j].index===fristLetter){
+                            cityList[j].list.push({nm:cities[i].nm,id:cities[i].id})
+                        }
+                    }
+                }
+            }
+
+            cityList.sort((n1,n2)=>{
+                if(n1.index>n2.index){
+                    return 1;
+                }else if(n1.index<n2.index){
+                    return -1;
+                }else{
+                    return 0
+                }
+            })
+
+            function toCom(fristLetter){
+                for(var i=0;i<cityList.length;i++){
+                if(cityList[i].index===fristLetter){
+                    return false;
+                }
+            }
+            return true;
+            }
+            //console.log(hotList);
+            return {
+                cityList,
+                hotList
+            };
+        },
+        handleToIndex(index){
+            var h2=this.$refs.city_sort.getElementsByTagName('h2');
+            this.$refs.city_sort.parentNode.scrollTop=h2[index].offsetTop;
+        }
+    }
 }
 </script>
 
